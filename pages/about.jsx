0f4bd9fe5069e638543bc340ba9f4068b31434b7
James Reagan <james@jpreagan.com>
2022-06-25 12:23:38 -1000

    Validate with prettier and eslint

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,9 @@
-import React from "react"
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function About() {
   return (
-
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About Page</h1>
@@ -15,5 +14,5 @@ export default function About() {
         </p>
       </main>
     </div>
-  )
+  );
 }
