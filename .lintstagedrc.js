ca747f51b2a4f1f87f3b75f0809b57a501acec48
Keegan <leary.keegan@gmail.com>
2022-06-25 19:45:02 -0700

    remove prettier --write

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,5 +7,9 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+<<<<<<< HEAD
   // "*.{js,jsx,ts,tsx,json,md,css}": ["prettier --write"],
+=======
+  //"*.{js,jsx,ts,tsx,json,md,css}": ["prettier --write"],
+>>>>>>> e7625a6 (remove prettier --write)
 };
