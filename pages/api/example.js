0f0b50872499efb81567fac8035f8962625156ab
James Reagan <james@jpreagan.com>
2022-06-27 09:06:23 -1000

    Enable connection to MongoDB

---
+++ pages/api/example.js
@@ -0,0 +1,17 @@
+import clientPromise from '../../lib/mongodb';
+
+export default async (req, res) => {
+  try {
+    const client = await clientPromise;
+    const db = await client.db();
+    const example = await db
+      .collection('example')
+      .find({})
+      .limit(20)
+      .toArray();
+    res.json(example);
+  } catch (error) {
+    console.error(error);
+    res.json({ error: 'Not connected!' });
+  }
+};
