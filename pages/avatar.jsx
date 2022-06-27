f9d256c69512f3454ac157a674f5fbb4838c99ef
Keegan <leary.keegan@gmail.com>
2022-06-27 15:04:25 -0700

    resetting to keegan3

--- pages/avatar.jsx
+++ pages/avatar.jsx
@@ -1,36 +1,34 @@
 import React, { useRef } from "react";
 import Link from "next/link";
 import Webcam from "react-webcam";
-
-import styles from "../styles/Home.module.css";
+import * as faceapi from "face-api.js";
 
 export default function Avatar() {
+  console.log(faceapi);
   const webcam = useRef(null);
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Build Your Avatar</h1>
-        <div className="">
-          <Webcam
-            audio={false}
-            ref={webcam}
-            style={{
-              position: "absolute",
-              margin: "auto",
-              textAlign: "center",
-              top: 100,
-              left: 0,
-              right: 0,
-            }}
-          />
-        </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Build Your Avatar</h1>
+      <div className="relative">
+        <Webcam
+          audio={false}
+          ref={webcam}
+          style={{
+            position: "absolute",
+            margin: "auto",
+            textAlign: "center",
+            top: 100,
+            left: 0,
+            right: 0,
+          }}
+        />
+      </div>
 
-        <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Go Back</a>
-          </Link>
-        </p>
-      </main>
-    </div>
+      <p>
+        <Link href="/">
+          <a>&larr; Go Back</a>
+        </Link>
+      </p>
+    </>
   );
 }
