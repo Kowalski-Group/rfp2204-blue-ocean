66ef5fe2aa53cb60ca0b1e38e4a3490776fcdcc5
Keegan <leary.keegan@gmail.com>
2022-06-27 13:33:30 -0700

    works well once I terminate the server...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-// module.exports = {
-//   webpack5: true,
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false };
-//     return config;
-//   },
-// };
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
