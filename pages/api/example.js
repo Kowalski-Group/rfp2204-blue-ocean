f9fbeedf991a9f7ece407670a72b018dd5dce0af
Keegan <leary.keegan@gmail.com>
2022-06-27 14:07:41 -0700

    verify I can push updates

--- pages/api/example.js
+++ pages/api/example.js
@@ -1,17 +1,14 @@
-import clientPromise from '../../lib/mongodb';
+import clientPromise from "../../lib/mongodb";
 
 export default async (req, res) => {
   try {
     const client = await clientPromise;
     const db = await client.db();
-    const example = await db
-      .collection('example')
-      .find({})
-      .limit(20)
-      .toArray();
+    const example = await db.collection("example").find({}).limit(20).toArray();
     res.json(example);
   } catch (error) {
+    // comment to verify I can push changes
     console.error(error);
-    res.json({ error: 'Not connected!' });
+    res.json({ error: "Not connected!" });
   }
 };
