b52a37e1d5c11cb46d2258c91f5488a7a43fd3c3
Michael Lin <michaelin.96@gmail.com>
2022-06-27 21:47:23 -0700

    Login Button in header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,49 +7,45 @@ function Header() {
   const { user } = useUser();
 
   return (
-    // <header className="p-1 bg-teal-500 text-white font-sans">
-    //   <div className="m-3 flex items-center">
-    //     <div className="">
-    //       <Link href="/">
-    //         <a className="text-xl ml-6">Home</a>
-    //       </Link>
-    //     </div>
-    //     <h1 className="text-3xl flex-1 text-center">Sweet Niblets</h1>
-    //     {user ? (
-    //       <div className="border-2 rounded-md hover:bg-white hover:text-teal-500">
-    //         <a
-    //           className="text-xl ml-3 mr-3"
-    //           href="/api/auth/logout"
-    //           data-testid="logout"
-    //         >
-    //           Logout
-    //         </a>
-    //       </div>
-    //     ) : (
-    //       <div className="border-2 rounded-md hover:bg-white hover:text-teal-500">
-    //         <a
-    //           className="text-xl ml-3 mr-3"
-    //           href="/api/auth/login"
-    //           data-testid="login"
-    //         >
-    //           Login
-    //         </a>
-    //       </div>
-    //     )}
-    <header className="bg-indigo-700 text-white py-6 px-4 grid grid-flow-col auto-cols-max gap-2">
-      <div className="h-20 w-20 relative ">
-        <Image
-          src="/../public/mic1.png"
-          alt="logo"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-full"
-        />
-      </div>
-      <div className="col-span-3">
-        <h1 className="text-4xl font-Audiowide"> Sweet Niblets</h1>
+    <header className="bg-indigo-700 text-white py-10 px-4">
+      <div className="flex flex-row justify-between">
+        <div className="grid grid-flow-col auto-cols-max gap-2">
+          <div className="h-20 w-20 relative">
+            <Image
+              src="/../public/mic1.png"
+              alt="logo"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full col-start-1"
+            />
+          </div>
+          <div className="col-span-3">
+            <h1 className="text-4xl font-Audiowide"> Sweet Niblets</h1>
 
-        <p>Let&apos;s sing together</p>
+            <p>Let&apos;s sing together</p>
+          </div>
+        </div>
+        {user ? (
+          <div className="self-center border-2 rounded-md hover:bg-white hover:text-indigo-700 ml-5">
+            <a
+              className="text-xl ml-3 mr-3"
+              href="/api/auth/logout"
+              data-testid="logout"
+            >
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="float-right top-12 right-10 self-center border-2 rounded-md hover:bg-white hover:text-indigo-700 justify-self-end ml-5">
+            <a
+              className="text-xl ml-3 mr-3"
+              href="/api/auth/login"
+              data-testid="login"
+            >
+              Login
+            </a>
+          </div>
+        )}
       </div>
     </header>
   );
