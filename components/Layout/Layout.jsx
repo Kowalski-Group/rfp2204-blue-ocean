fe21ec9db5e731836509105bbd0afaebcbf7624b
skydodle <skydodle@gmail.com>
2022-06-28 15:11:42 -0700

    added layout to play

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Head from "next/head";
+import PropTypes from "prop-types";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-function Layout() {
+function Layout({ children }) {
   return (
     <div>
       <Head>
@@ -12,11 +13,22 @@ function Layout() {
       </Head>
       <Header />
       <body className="flex flex-col min-h-screen">
-        <main className="flex-grow">this is main</main>
+        <main className="flex-grow">{children}</main>
       </body>
       <Footer />
     </div>
   );
 }
+
+// Layout.propTypes = {
+//   children: PropTypes.oneOfType([
+//     PropTypes.arrayOf(PropTypes.node),
+//     PropTypes.node,
+//   ]).isRequired,
+// };
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Layout;
--- pages/play.jsx
+++ pages/play.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import YouTube from "react-youtube";
 import Image from "next/image";
 
+import Layout from "../components/Layout/Layout";
+
 export default function play() {
   const opts = {
     playerVars: {
@@ -13,18 +15,20 @@ export default function play() {
   };
 
   return (
-    <div className="relative w-[100vw] h-[100vh] flex items-center justify-center">
-      <Image
-        className="object-cover w-full h-60 scale-125"
-        src="/../public/karaoke-background.jpg"
-        alt="logo"
-        layout="fill"
-      />
-      <YouTube
-        className="absolute top-[40%] object-cover mx-auto"
-        videoId="rMSQwIp4Jg8"
-        opts={opts}
-      />
-    </div>
+    <Layout>
+      <div className="relative w-[100vw] h-[100vh] flex items-center justify-center">
+        <Image
+          className="object-cover w-full h-60 scale-125"
+          src="/../public/karaoke-background.jpg"
+          alt="logo"
+          layout="fill"
+        />
+        <YouTube
+          className="absolute top-[40%] object-cover mx-auto"
+          videoId="rMSQwIp4Jg8"
+          opts={opts}
+        />
+      </div>
+    </Layout>
   );
 }
