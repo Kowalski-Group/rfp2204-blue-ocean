d6420f50d43f81d9c6c79d5c64a8095002655f7a
skydodle <skydodle@gmail.com>
2022-06-27 17:52:51 -0700

    added logo and app name

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 function Header() {
   return (
-    <header className="bg-indigo-700 text-white py-6 px-4">
-      <h1 className="text-2xl"> Logo</h1>
-      <p>this a Header</p>
+    <header className="bg-indigo-700 text-white py-6 px-4 grid grid-cols-12 gap-0">
+      <div className="h-20 w-20 relative ">
+        <Image
+          src="/../public/mic1.png"
+          alt="logo"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
+      </div>
+      <div className="col-span-3">
+        <h1 className="text-4xl  font-Audiowide"> Sweet Niblets</h1>
+
+        <p>Let&apos;s sing together</p>
+      </div>
     </header>
   );
 }
