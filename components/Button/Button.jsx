22798a0f9f627072e2e0cadbdeadd2be40ff1bd5
Keegan <leary.keegan@gmail.com>
2022-06-30 10:01:30 -0700

    bring back the confetti

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Button() {
-  return <button type="button">this is button</button>;
+function Button({ buttonText, handleClick }) {
+  return (
+    <button
+      type="button"
+      className="bg-indigo-50 text-indigo-900 px-4 py-2 text-lg lowercase rounded-full hover:bg-indigo-200 transition-colors duration-200 ease-out"
+      onClick={handleClick}
+    >
+      {buttonText}
+    </button>
+  );
 }
 
 export default Button;
+
+Button.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
--- pages/play2.jsx
+++ pages/play2.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
-// import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
+import { useWindowSize } from "react-use";
 import RecordButton from "../components/RecordButton";
 import AudioDrawer from "../components/AudioDrawer";
-
 import Layout from "../components/Layout/Layout";
 import useRecorder from "./api/useRecorder";
 import Avatar from "../components/Avatar";
+import Button from "../components/Button/Button";
 
 export default function Play() {
+  const { width, height } = useWindowSize();
+
   // const { width, height } = useWindowSize();
   const [audioURL, isRecording, startRecording, stopRecording] = useRecorder();
   const [showAudioDrawer, setShowAudioDrawer] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
 
   const handleRecordClicked = () => {
     if (!isRecording) {
@@ -23,11 +27,15 @@ export default function Play() {
     }
   };
 
+  const handleShowConfettiClicked = () => {
+    setShowConfetti(!showConfetti);
+  };
+
   return (
     <Layout>
       <div className="relative bg-indigo-100 flex flex-col items-center px-[20%] pt-36 h-[100vh] overflow-hidden">
         <Avatar />
-        {/* <Confetti width={width} height={height} /> */}
+        {showConfetti && <Confetti width={width} height={height} />}
         <div className="video-wrapper">
           <iframe
             className="video-element rounded-3xl"
@@ -38,6 +46,12 @@ export default function Play() {
         </div>
         {showAudioDrawer && <AudioDrawer audioURL={audioURL} />}
         <RecordButton handleClick={handleRecordClicked} />
+        <div className="absolute left-2 bottom-2 flex flex-col">
+          <Button
+            handleClick={handleShowConfettiClicked}
+            buttonText={showConfetti ? "🚫 Confetti" : "🎊 Confetti"}
+          />
+        </div>
       </div>
     </Layout>
   );
